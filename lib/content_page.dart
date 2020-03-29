554a5a07f25baed608e1e72380437f56e1f2db85
hengfeng.zhuo <hengfeng.zhuo@rokid.com>
2020-03-29 13:00:00 +0800

    [feat] 实现内容区域和tab区域联动

---
+++ lib/content_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+/// 显示内容部分
+class ContentPage extends StatefulWidget {
+  // 定义page滑动的回调
+  final ValueChanged<int> onPageChanged;
+  final ContentPageController contentPageController;
+
+  const ContentPage({Key key, this.onPageChanged, this.contentPageController}) : super(key: key);
+
+  @override
+  _ContentPageState createState() => _ContentPageState();
+}
+
+class _ContentPageState extends State<ContentPage> {
+  static List<Color> _colors = [
+    Colors.blue,
+    Colors.lightGreen,
+    Colors.amberAccent,
+    Colors.orange
+  ];
+
+  PageController _pageController = PageController(
+    viewportFraction: 0.8 // 设置视图比例
+  );
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    if (widget.contentPageController != null) {
+      widget.contentPageController._pageController = this._pageController;
+    }
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        // 搜索栏 appbar
+
+        // 内容栏
+        // 使用Expanded权重来分配子View的空间占比
+        // 高度撑开，否则在column中没有指定高度会报错
+        new Expanded(
+            child: new PageView(
+              controller: _pageController,
+              onPageChanged: widget.onPageChanged,
+              children: <Widget>[
+                _wrapItem(0),
+                _wrapItem(1),
+                _wrapItem(2),
+                _wrapItem(3),
+              ],
+            ),
+        ),
+
+      ],
+    );
+  }
+
+  Widget _wrapItem(int index) {
+    return new Padding(
+        padding: EdgeInsets.all(10),
+        child: Container(
+          decoration: BoxDecoration(color: _colors[index]),
+        ),
+    );
+  }
+}
+
+/// 内容区域控制器
+class ContentPageController {
+    PageController _pageController;
+
+    void jumpToPage(int page) {
+      _pageController?.jumpToPage(page);
+    }
+}
--- lib/tab_navigator.dart
+++ lib/tab_navigator.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'content_page.dart';
+
 /// 底部导航条
 class TabNavigator extends StatefulWidget {
   @override
@@ -10,6 +12,7 @@ class _TabNavigatorState extends State<TabNavigator> {
   final _defaultColor = Colors.grey;
   final _activeColor = Colors.blue;
   int _currentIndex = 0;
+  final ContentPageController _contentPageController = ContentPageController();
 
   @override
   Widget build(BuildContext context) {
@@ -26,17 +29,22 @@ class _TabNavigatorState extends State<TabNavigator> {
             end: Alignment.bottomCenter,
           ),
         ),
-        child: Center(
-          child: Text(
-            '你点击了第 $_currentIndex 个tab',
-            style: TextStyle(fontSize: 36, color: Colors.blue),
-          ),
+        child: ContentPage(
+          contentPageController: _contentPageController,
+          onPageChanged: (index) {
+            // 修改状态
+            setState(() {
+              _currentIndex = index;
+            });
+          },
         ),
       ),
       /// 底部导航条
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _currentIndex,
         onTap: (index) {
+          // 点击tab，滑动viewpage
+          _contentPageController.jumpToPage(index);
           // 修改状态
           setState(() {
             _currentIndex = index;
@@ -47,7 +55,7 @@ class _TabNavigatorState extends State<TabNavigator> {
           _bottomItem('本周', Icons.folder, 0),
           _bottomItem('分享', Icons.explore, 1),
           _bottomItem('免费', Icons.donut_small, 2),
-          _bottomItem('长按', Icons.person, 3),
+          _bottomItem('长安', Icons.person, 3),
         ],
         type: BottomNavigationBarType.fixed,
       ),
